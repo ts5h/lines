6fa443d24ac7b2f9ced5f1a1278ef5929f565ae5
at4 <at4@jc4.so-net.ne.jp>
2023-03-11 01:15:47 +0100

    Add css for component

--- src/components/Lines/index.tsx
+++ src/components/Lines/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import Styles from "../../scss/Lines.module.scss";
 
 export const Lines = () => {
-  return <div>Lines</div>;
+  return <div className={Styles.lines}>Lines</div>;
 };
