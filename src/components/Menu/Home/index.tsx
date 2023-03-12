b5e7119e449e63b5a685b5ff12ec6b4d1bc2a671
at4 <at4@jc4.so-net.ne.jp>
2023-03-12 22:04:15 +0100

    Update MenuHome component

--- src/components/Menu/Home/index.tsx
+++ src/components/Menu/Home/index.tsx
@@ -3,5 +3,18 @@ import { ArrowBackToHome } from "../../../icons";
 import Styles from "../../../scss/MenuHome.module.scss";
 
 export const MenuHome = () => {
-  return <div className={Styles.wrapper}>test</div>;
+  const handleHome = () => {
+    window.location.href = "/";
+  };
+
+  return (
+    <div className={Styles.wrapper}>
+      <button type={"button"} className={Styles.menu} onClick={handleHome}>
+        <div className={Styles.icon}>
+          <ArrowBackToHome />
+        </div>
+        <p>HOME</p>
+      </button>
+    </div>
+  );
 };
