32edb1e7baec9182f5d17f8265e0d831a1391562
at4 <at4@jc4.so-net.ne.jp>
2023-03-12 15:46:08 +0100

    Add sound on / off functions

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { GiSpeaker, GiSpeakerOff } from "react-icons/gi";
 import { AiFillGithub, AiOutlineReload } from "react-icons/ai";
+import { useAtom } from "jotai";
+import { soundFlagAtom } from "../../store/Atoms";
 import Styles from "../../scss/Menu.module.scss";
 
 export const Menu = () => {
+  const [isSound, setIsSound] = useAtom(soundFlagAtom);
+
   const handleSound = () => {
-    // sound
+    setIsSound((prev) => !prev);
   };
 
   const handleReload = () => {
@@ -20,7 +24,11 @@ export const Menu = () => {
     <div className={Styles.wrapper}>
       <nav className={Styles.nav}>
         <button type={"button"} className={Styles.sound} onClick={handleSound}>
-          <GiSpeaker className={`${Styles.icon} ${Styles.sound}`} title={"Sound On / Off"} />
+          {isSound ? (
+            <GiSpeaker className={`${Styles.icon} ${Styles.sound}`} title={"Sound On / Off"} />
+          ) : (
+            <GiSpeakerOff className={`${Styles.icon} ${Styles.sound}`} title={"Sound On / Off"} />
+          )}
         </button>
         <button type={"button"} onClick={handleReload}>
           <AiOutlineReload className={`${Styles.icon} ${Styles.reload}`} title={"Reload"} />
