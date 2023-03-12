51cd6fff23b438fac1c25e2f65bae49242d58c0e
at4 <at4@jc4.so-net.ne.jp>
2023-03-12 15:22:18 +0100

    Add function to each tool button

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,32 @@
 import React from "react";
-import { AiFillGithub } from "react-icons/ai";
+import { GiSpeaker, GiSpeakerOff } from "react-icons/gi";
+import { AiFillGithub, AiOutlineReload } from "react-icons/ai";
 import Styles from "../../scss/Menu.module.scss";
 
 export const Menu = () => {
+  const handleSound = () => {
+    // sound
+  };
+
+  const handleReload = () => {
+    window.location.reload();
+  };
+
+  const handleGitHub = () => {
+    window.open("https://github.com/ts5h/lines");
+  };
+
   return (
     <div className={Styles.wrapper}>
       <nav className={Styles.nav}>
-        <button type={"button"}>
-          <i className={`${Styles.icon} ${Styles.sound}`}>Sound</i>
+        <button type={"button"} className={Styles.sound} onClick={handleSound}>
+          <GiSpeaker className={`${Styles.icon} ${Styles.sound}`} />
         </button>
-        <button type={"button"}>
-          <i className={`${Styles.icon} ${Styles.reload}`}>Reload</i>
+        <button type={"button"} onClick={handleReload}>
+          <AiOutlineReload className={`${Styles.icon} ${Styles.reload}`} />
         </button>
-        <button type={"button"}>
-          <i className={`${Styles.icon} ${Styles.github}`}>
-            <AiFillGithub />
-          </i>
+        <button type={"button"} onClick={handleGitHub}>
+          <AiFillGithub className={`${Styles.icon} ${Styles.reload}`} />
         </button>
       </nav>
     </div>
