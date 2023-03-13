992cd6b5447a3d38c7f2c61ba3569ed8b076bd7d
at4 <at4@jc4.so-net.ne.jp>
2023-03-13 21:24:53 +0100

    Update button interaction for mobile

--- src/components/Menu/Home/index.tsx
+++ src/components/Menu/Home/index.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { isMobile } from "react-device-detect";
 import { ArrowBackToHome } from "../../../icons";
 import Styles from "../../../scss/MenuHome.module.scss";
 
 export const MenuHome = () => {
-  const handleHome = () => {
+  const [isHover, setIsHover] = useState(false);
+
+  const handleHover = (state: boolean) => {
+    if (isMobile) return;
+    setIsHover(state);
+  };
+
+  const handleTouch = (state: boolean) => {
+    if (!isMobile) return;
+    setIsHover(state);
+  };
+
+  const handleClick = () => {
     window.location.href = "/";
   };
 
   return (
     <div className={Styles.wrapper}>
-      <button type={"button"} className={Styles.menu} onClick={handleHome}>
+      <button
+        type={"button"}
+        className={`${Styles.menu} ${isHover ? Styles.on : ""}`}
+        onMouseOver={() => handleHover(true)}
+        onMouseOut={() => handleHover(false)}
+        onFocus={() => handleHover(true)}
+        onBlur={() => handleHover(false)}
+        onTouchStart={() => handleTouch(true)}
+        onTouchEnd={() => handleTouch(false)}
+        onClick={handleClick}
+      >
         <div className={Styles.icon}>
           <ArrowBackToHome />
         </div>
