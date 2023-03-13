1052228a9d9b640ab683fc05cc408be84fdb9d05
at4 <at4@jc4.so-net.ne.jp>
2023-03-13 01:34:10 +0100

    Update sound processing

--- src/hooks/usePlaySound/index.ts
+++ src/hooks/usePlaySound/index.ts
@@ -20,20 +20,24 @@ export const usePlaySound = () => {
       const seventh = isBass ? [midiNumber] : [midiNumber, midiNumber + 3, midiNumber + 7, midiNumber + 10];
       const duration = isBass ? (10 - speed) * 0.5 : (10 - speed) * 0.25;
 
+      const comp = new DynamicsCompressorNode(audioCtx, {
+        threshold: -8,
+        knee: 36,
+        ratio: 16,
+        attack: 0.01,
+        release: 1.0,
+      });
+
       seventh.forEach((tone) => {
         const osc = new OscillatorNode(audioCtx);
         const amp = new GainNode(audioCtx);
-        const comp = new DynamicsCompressorNode(audioCtx, {
-          threshold: -36,
-          knee: 40,
-          ratio: 20,
-          attack: 0.01,
-          release: 1.0,
-        });
 
         osc.type = "sine";
         osc.frequency.value = getFrequency(tone);
-        amp.gain.value = 0.05;
+        // NOTE: Cut super bass
+        if (osc.frequency.value <= 55) return;
+
+        amp.gain.value = osc.frequency.value < 120 ? 0.0001 : 0.01;
 
         osc.connect(amp).connect(comp).connect(audioCtx.destination);
         osc.start(audioCtx.currentTime);
