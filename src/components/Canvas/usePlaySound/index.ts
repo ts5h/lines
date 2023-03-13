23f4663a09cbd0f27fd9d40066326d8b7b2b46ee
at4 <at4@jc4.so-net.ne.jp>
2023-03-13 11:39:47 +0100

    Refactor usePlaySound hook

--- src/components/Canvas/usePlaySound/index.ts
+++ src/components/Canvas/usePlaySound/index.ts
@@ -11,16 +11,16 @@ export const usePlaySound = () => {
     return 2 ** ((midiNumber - 69) / 12) * 440;
   }, []);
 
-  const audioCtx = useMemo(() => new AudioContext(), []);
+  const audioContext = useMemo(() => new AudioContext(), []);
 
   const playSound = useCallback(
     (midiNumber: number, speed: number, isBass: boolean) => {
       if (!isSound) return;
 
-      const seventh = isBass ? [midiNumber] : [midiNumber, midiNumber + 3, midiNumber + 7, midiNumber + 10];
+      const chord = isBass ? [midiNumber] : [midiNumber, midiNumber + 3, midiNumber + 7, midiNumber + 10];
       const duration = isBass ? (10 - speed) * 0.5 : (10 - speed) * 0.25;
 
-      const comp = new DynamicsCompressorNode(audioCtx, {
+      const compressor = new DynamicsCompressorNode(audioContext, {
         threshold: -8,
         knee: 36,
         ratio: 16,
@@ -28,26 +28,26 @@ export const usePlaySound = () => {
         release: 1.0,
       });
 
-      seventh.forEach((tone) => {
-        const osc = new OscillatorNode(audioCtx);
-        const amp = new GainNode(audioCtx);
+      chord.forEach((tone) => {
+        const oscillator = new OscillatorNode(audioContext);
+        const gain = new GainNode(audioContext);
 
-        osc.type = "sine";
-        osc.frequency.value = getFrequency(tone);
+        oscillator.type = "sine";
+        oscillator.frequency.value = getFrequency(tone);
         // NOTE: Cut super bass
-        if (osc.frequency.value <= 55) return;
+        if (oscillator.frequency.value <= 55) return;
 
-        amp.gain.value = osc.frequency.value < 120 ? 0.0001 : 0.01;
+        gain.gain.value = oscillator.frequency.value < 120 ? 0.0001 : 0.01;
 
-        osc.connect(amp).connect(comp).connect(audioCtx.destination);
-        osc.start(audioCtx.currentTime);
-        osc.stop(audioCtx.currentTime + duration);
+        oscillator.connect(gain).connect(compressor).connect(audioContext.destination);
+        oscillator.start(audioContext.currentTime);
+        oscillator.stop(audioContext.currentTime + duration);
 
-        amp.gain.setValueAtTime(0.2, audioCtx.currentTime);
-        amp.gain.exponentialRampToValueAtTime(0.00001, audioCtx.currentTime + duration);
+        gain.gain.setValueAtTime(0.2, audioContext.currentTime);
+        gain.gain.exponentialRampToValueAtTime(0.00001, audioContext.currentTime + duration);
       });
     },
-    [audioCtx, getFrequency, isSound]
+    [audioContext, getFrequency, isSound]
   );
 
   return {
