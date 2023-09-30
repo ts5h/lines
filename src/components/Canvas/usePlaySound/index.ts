0ab9fde2ae84922e0d70cc0eab8ade27064b8637
at4 <at4@jc4.so-net.ne.jp>
2023-09-30 19:15:32 +0200

    Update logic and sounds

--- src/components/Canvas/usePlaySound/index.ts
+++ src/components/Canvas/usePlaySound/index.ts
@@ -3,8 +3,8 @@ import { useAtom } from "jotai";
 import { audioContextAtom, soundFlagAtom } from "../../../store/Atoms";
 
 export const usePlaySound = () => {
-  const [audioContext, setAudioContext] = useAtom<AudioContext>(audioContextAtom);
-  const [isSound, setIsSound] = useAtom(soundFlagAtom);
+  const [audioContext] = useAtom<AudioContext>(audioContextAtom);
+  const [isSound] = useAtom(soundFlagAtom);
 
   const getFrequency = useCallback((midiNumber: number) => {
     return 2 ** ((midiNumber - 69) / 12) * 440;
@@ -14,11 +14,13 @@ export const usePlaySound = () => {
     (midiNumber: number, speed: number, isBass: boolean) => {
       if (!isSound) return;
 
-      const chord = isBass ? [midiNumber] : [midiNumber, midiNumber + 3, midiNumber + 7, midiNumber + 10];
+      const chord = isBass
+        ? [midiNumber]
+        : [midiNumber, midiNumber + 3, midiNumber + 7, midiNumber + 10];
       const duration = isBass ? (10 - speed) * 0.5 : (10 - speed) * 0.25;
 
       const compressor = new DynamicsCompressorNode(audioContext, {
-        threshold: -8,
+        threshold: -10,
         knee: 36,
         ratio: 16,
         attack: 0.01,
@@ -34,15 +36,18 @@ export const usePlaySound = () => {
         // NOTE: Cut super bass
         if (oscillator.frequency.value <= 55) return;
 
-        gain.gain.value = oscillator.frequency.value < 120 ? 0.0001 : 0.01;
-        gain.gain.value = 1.0;
-
-        oscillator.connect(gain).connect(compressor).connect(audioContext.destination);
+        oscillator
+          .connect(gain)
+          .connect(compressor)
+          .connect(audioContext.destination);
         oscillator.start(audioContext.currentTime);
         oscillator.stop(audioContext.currentTime + duration);
 
-        gain.gain.setValueAtTime(0.2, audioContext.currentTime);
-        gain.gain.exponentialRampToValueAtTime(0.00001, audioContext.currentTime + duration);
+        gain.gain.setValueAtTime(0.4, audioContext.currentTime);
+        gain.gain.exponentialRampToValueAtTime(
+          0.00001,
+          audioContext.currentTime + duration,
+        );
 
         oscillator.onended = () => {
           oscillator.disconnect();
